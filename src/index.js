c659a9900083ce9b32936e4eace98ce83377c2c9
brendon <200~108491991+BrendonTheDev@users.noreply.github.com>
2023-06-05 13:10:49 -0400

    DOM element creating working.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import _ from "lodash";
+import "./style.css";
+import Food from "./food.jpeg";
 
 function component() {
-  const element = document.createElement("div");
+  const container = document.getElementById("content");
 
   // Lodash, now imported via above script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  let header = document.createElement("h1");
+  header.textContent = "Deja's Diner";
+
+  let paragraph = document.createElement("p");
+  paragraph.textContent =
+    "Wow! A diner run by cats that feeds cats! Catlorem Catnipsum.";
+
+  const myImage = new Image();
+  myImage.src = Food;
+
+  container.classList.add("hello");
+
+  container.appendChild(header);
+  container.appendChild(paragraph);
+  container.appendChild(myImage);
 
   return element;
 }
-
-document.body.appendChild(component());
+component();
+// document.body.appendChild(component());
