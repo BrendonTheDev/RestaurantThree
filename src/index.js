2e82801ac7b58b663e14334a044781bb0da079df
brendon <200~108491991+BrendonTheDev@users.noreply.github.com>
2023-06-05 13:15:18 -0400

    build works! using component func module imported into index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import _ from "lodash";
-import "./style.css";
-import Food from "./food.jpeg";
 
-function component() {
-  const container = document.getElementById("content");
+import { component } from "./component.js";
 
-  // Lodash, now imported via above script
-  let header = document.createElement("h1");
-  header.textContent = "Deja's Diner";
-
-  let paragraph = document.createElement("p");
-  paragraph.textContent =
-    "Wow! A diner run by cats that feeds cats! Catlorem Catnipsum.";
-
-  const myImage = new Image();
-  myImage.src = Food;
-
-  container.classList.add("hello");
-
-  container.appendChild(header);
-  container.appendChild(paragraph);
-  container.appendChild(myImage);
-
-  return element;
-}
 component();
 // document.body.appendChild(component());
